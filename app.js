e91129251de7694ed8edc3f73eadbe0531e17a9a
Aure77 <aure77@prozero.fr>
2014-07-05 11:49:27 +0200

    Gestion recherche carte

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/tournaments', tournaments);
-app.use('/heathstone-decks', hearthstoneDecks);
+app.use('/hearthstone-decks', hearthstoneDecks);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/hearthstone-decks.js
+++ routes/hearthstone-decks.js
@@ -3,15 +3,21 @@ var escape = require('escape-html');
 var hearthstonejson = require('../modules/hearthstone-card-search');
 var router = express.Router();
 
-/*router.get('/', function(req, res) {
-  hearthstonejson.findCardById(req.param.cardName, function(card) {
-    res.render('index', { title: 'Listes des decks', card: {}});
+router.get('/', function(req, res) {
+  hearthstonejson.findAllCards(function(cards) {
+    console.log("cards="+cards.length);
+    var d = [];
+    for(i=0;i<6;i++) {
+        d.push({id: i, name: "Test"+i});
+    }
+    res.render('hearthstone-decks', { title: 'Cartes', cards: cards});
   });
-});*/
+});
 
 /* GET search by name */
 router.get('/search/name/:cardName', function(req, res) {
   hearthstonejson.findCardByName(req.params.cardName, function(card) {
+    console.log(card);
     res.render('hearthstone-decks', { title: 'Carte', card: card});
   });
 });
