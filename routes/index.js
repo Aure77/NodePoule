af8ef28dee4309f1e552d4ed66c85adfd9dc1d17
Mohye77 <mohye@orange.fr>
2014-07-06 19:50:04 +0200

    no more card on index

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,4 @@ var express = require('express');
 var router = express.Router();
 var wowhead = require('wowhead')();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  var cardName = req.param('q');
-  wowhead.getCard(cardName, function(err, card) { 
-    if(typeof card == 'undefined') {
-        res.status(404).send('Not card found with name : ' + cardName);
-    } else {
-        console.log(card);
-        res.render('index', { title: 'HearthStone', card: card });
-    }
-  });  
-});
-
 module.exports = router;
