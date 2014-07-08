d38e7fa53cba2da19b9cc8180688268b9c4ad7ba
Mohye77 <mohye@orange.fr>
2014-07-08 17:55:17 +0200

    correction liens

--- routes/competitions.js
+++ routes/competitions.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('competitions', { title: 'Compétitions' });
+  res.render('competitions', { title: 'Compétitions', competitions:[] });
 });
 
 module.exports = router;
