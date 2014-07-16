07a520c5731bc31a5ffef8fa247f090d875121ad
Mohye77 <mohye@orange.fr>
2014-07-16 22:12:45 +0200

    mega test

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var mongoose = require('mongoose'), Tournament = mongoose.model('Tournament');
 var router = express.Router();
+var today = new Date();
 
 router.get(['/', '/index.html'], function(req, res) {
+  Tournament.find({ today:{$gte:'startDate', $lte:'endDate' }}, function(err, tournaments) {
+    if (err) { return next(err); }
+    res.render('tournaments', { 
+      title: 'Listes des tournois', 
+      tournaments: tournaments
+    });
+  });
   res.render('index', { title: 'Tournois en cours' });
 });
 
