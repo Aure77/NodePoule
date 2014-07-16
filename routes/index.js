ae8dff8ecf47cadcc15a73548d5ba57b9f41d02f
Aure77 <aure77@prozero.fr>
2014-07-16 21:40:10 +0200

    AJout page de contacts

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get(['/', '/index.html'], function(req, res) {
-  console.log("req.user="+req.user);
   res.render('index', { title: 'Tournois en cours' });
+});
+
+router.get(['/contacts.html'], function(req, res) {
+  res.render('index', { title: 'Contactez-nous' });
 });
 
 module.exports = router;
