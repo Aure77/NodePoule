ee478e426a650a1f99afec803c25d1ca6fdf4d65
Aure77 <aure77@prozero.fr>
2014-07-21 22:04:11 +0200

    Liste des participants avec Challonge

--- routes/tournaments.js
+++ routes/tournaments.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var util = require('util');
+var rest = require('restler');
+var nconf = require('nconf');
 var escape = require('escape-html');
 var paginate = require('express-paginate');
 var mongoose = require('mongoose'), Tournament = mongoose.model('Tournament');
@@ -23,6 +26,26 @@ router.get('/:id', function(req, res, next) {
   Tournament.findOne({ tournamentId : escape(req.params.id) }).populate("game").exec(function(err, tournament) {
     if (err) { return next(err); }
     res.render('tournament', { title: tournament.name, tournament: tournament });
+  });
+});
+
+router.get('/:id/participants', function(req, res, next) {
+  var challongeUrl = util.format('https://api.challonge.com/v1/tournaments/%s/participants.json?api_key=%s', escape(req.params.id), nconf.get('challonge_api_key'));
+  rest.json(challongeUrl, {timeout: 5000}).on('timeout', function(ms) {
+    return next(new Error('Challonge did not return within ' + ms + ' ms'));
+  }).on('complete',function(challongeParticipants, response) {
+    var participants = [];
+    challongeParticipants.forEach(function(chalParticipant) {
+      participants.push({
+        id: "challonge:" + chalParticipant.participant.id,
+        pseudo: (chalParticipant.participant.username != null) ? chalParticipant.participant.username : chalParticipant.participant.name,
+        avatarRelPath: util.format('http://www.gravatar.com/avatar/%s?r=r&s=200&d=http://mohye.eu/img/avatar.png', chalParticipant.participant.email_hash),
+        nb1stPlace: 0,
+        nb2ndPlace: 0,
+        nb3rdPlace: 0
+      });
+    });
+    res.render('participants', { title: "Participants", participants: participants });
   });
 });
 
