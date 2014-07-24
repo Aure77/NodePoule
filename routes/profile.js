e4673ea459297da09e12cf1252363b55034d060a
Aure77 <aure77@prozero.fr>
2014-07-24 20:30:23 +0200

    Gestion du profil utilisateur

--- routes/profile.js
+++ routes/profile.js
@@ -3,18 +3,38 @@ var escape = require('escape-html');
 var mongoose = require('mongoose'), UserProfil = mongoose.model('UserProfil'), User = mongoose.model('User');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  try {
+    findUserProfilById(req.user, function(userProfil) {
+      res.render('profile', { profil: userProfil });
+    });
+  } catch(e) {
+    next(e);
+  }
+});
+
 router.get('/:id', function(req, res, next) {
-  UserProfil.findOne({ uid: escape(req.params.id) })
+  try {
+    findUserProfilById(escape(req.params.id), function(userProfil) {
+      res.render('profile', { profil: userProfil });
+    });
+  } catch(e) {
+    next(e);
+  }
+});
+
+var findUserProfilById = function(uid, callback) {
+  UserProfil.findOne({ uid: uid })
             .populate('user', '_key uid username picture gravatarpicture uploadedpicture status')
             .populate('gamerTags.game')
             .lean()
             .exec(function(err, userProfil) {
     if(!userProfil) {
       //TODO if User model exist in objects collection(nodebb) -> create a new default profil
-      next(new Error("User '"+escape(req.params.id)+"' not found"));
-    }
-    res.render('profile', { profil: userProfil });
+      throw new Error("User '"+uid+"' not found");
+    }    
+    callback(userProfil);    
   });
-});
+}
 
 module.exports = router;
