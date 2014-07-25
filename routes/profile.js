c5ffedad62a7db1e64132822423dd294a703480d
Aure77 <aure77@prozero.fr>
2014-07-25 22:00:02 +0200

    Creation auto du profil utilisateur si inexistant

--- routes/profile.js
+++ routes/profile.js
@@ -4,30 +4,53 @@ var mongoose = require('mongoose'), UserProfil = mongoose.model('UserProfil'), U
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  findUserProfilById(req.user, function(err, userProfil) {
+  findOrCreateUserProfilByUserId(req.user, function(err, userProfil) {
     if (err) { return next(err); }
     res.render('profile', { profil: userProfil });
   });
 });
 
 router.get('/:id', function(req, res, next) {
-  findUserProfilById(escape(req.params.id), function(err, userProfil) {
+  findOrCreateUserProfilByUserId(escape(req.params.id), function(err, userProfil) {
     if (err) { return next(err); }
     res.render('profile', { profil: userProfil });
   });
 });
 
-function findUserProfilById(uid, callback) {
+function findOrCreateUserProfilByUserId(uid, callback) {
   UserProfil.findOne({ uid: uid })
             .populate('user', '_key uid username picture gravatarpicture uploadedpicture status')
             .populate('gamerTags.game')
             .lean()
             .exec(function(err, userProfil) {
-    if(!userProfil) {
-      //TODO if User model exist in objects collection(nodebb) -> create a new default profil
-      callback(new Error("UserProfil '"+uid+"' not found"), null);
-    }    
-    callback(null, userProfil);    
+    if (err) { return callback(err, null); }
+    
+    if(userProfil) {
+      callback(null, userProfil);
+    } else {
+      createDefaultUserProfil(uid, callback);
+    }
+  });
+}
+
+function createDefaultUserProfil(uid, callback) {
+  User.findOne({ uid: uid }, '_id uid').lean().exec(function(err, user) {
+    if (err) { return callback(err, null); }
+    if(!user) {
+      return callback(new Error("Le profil '"+uid+"' est introuvable"), null);
+    }
+    var defaultUserProfil = new UserProfil();
+    defaultUserProfil.uid = user.uid;
+    defaultUserProfil.user = user;
+    defaultUserProfil.gamerTags = [];
+    defaultUserProfil.save(function(err) {
+      if (err) { return callback(err, null); }
+      UserProfil.populate(defaultUserProfil, { path: 'user', select: '_key uid username picture gravatarpicture uploadedpicture status' }, function(err, userProfil) {
+        if (err) { return callback(err, null); }
+        console.log("UserProfil '"+userProfil.uid+"' successfully created");
+        callback(null, userProfil);
+      });
+    });
   });
 }
 
