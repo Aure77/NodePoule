a1b1d8bed4fbb6e4fc69c4585f46579eb9d7ccf7
Aure77 <aure77@prozero.fr>
2014-08-01 11:24:08 +0200

    MAJ date à chaque chargement de page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var mongoose = require('mongoose'), Tournament = mongoose.model('Tournament'), News = mongoose.model('News');
 var router = express.Router();
-var today = new Date();
 
 router.get(['/', '/index.html'], function(req, res, next) {
+  var today = new Date();
   Tournament.find({}).where('startDate').lte(today).where('endDate').gte(today).exec(function(err, tournaments) {
     if (err) { return next(err); }
     News.find({}).sort('-date').limit(8).exec(function(err2, newsCollection){
