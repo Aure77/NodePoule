762b84693f42a7a1bc79f06c1d434eb7721d98a1
Aure77 <aure77@prozero.fr>
2015-01-27 11:09:35 +0100

    Fix unexpected "}"

--- routes/faq.js
+++ routes/faq.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var escape = require('escape-html');
 var paginate = require('express-paginate');
-var mongoose = require('mongoose'), Games = mongoose.model('Games');
+var mongoose = require('mongoose'), Game = mongoose.model('Game');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	Games.find({},"name iconRelPath gameId").exec(function(err, games){
+	Game.find({},"name iconRelPath gameId").exec(function(err, games){
 		res.render('faq', { title: "FAQ", htitle: "La Foire Aux Questions!", games: games });
-	}
+	});
 });
 
 module.exports = router;
