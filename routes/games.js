6b3f885e6bf2767aad2c81399ac87913c4f9aaea
Mohye77 <mohye@orange.fr>
2015-01-29 23:00:13 +0100

    essai listing tournois pour un jeu

--- routes/games.js
+++ routes/games.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var escape = require('escape-html');
 var paginate = require('express-paginate');
-var mongoose = require('mongoose'), Game = mongoose.model('Game');
+var mongoose = require('mongoose'), Game = mongoose.model('Game'), Tournament =  = mongoose.model('tournament');
 var router = express.Router();
 
 router.get('/:id', function(req, res, next) {
@@ -13,6 +13,20 @@ router.get('/:id', function(req, res, next) {
     }
 		res.render('game', { title: "Jeu", htitle: "A propos de " + game.name, game: game });
 	});
+});
+
+router.get('/:id/tournaments', function(req, res, next) {
+    var gameId = escape(req.params.id);
+    Tournament.paginate({game._id : gameId}, escape(req.query.page), escape(req.query.limit), function(err, pageCount, tournaments, itemCount) {
+    if (err) { return next(err); }
+    res.render('tournaments', { 
+      title: 'Tous les tournois', 
+      htitle: 'Tous les tournois',
+      tournaments: tournaments, 
+      pageCount: pageCount,
+      itemCount: itemCount 
+    });
+  });
 });
 
 module.exports = router;
